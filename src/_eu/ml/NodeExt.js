f9c5e9248dbdf8990aee3f35dfcee05f91eff5fb
Khanh Nguyen <kn@visionarity.com>
2016-01-29 18:51:14 +0100

    Almost done with NodeExt.js

--- src/_eu/ml/NodeExt.js
+++ src/_eu/ml/NodeExt.js
@@ -1,62 +1,62 @@
 //Define namespace
 var EU = EU || {};
 
-EU.NodeExt = cc.Class.extend({
+EU.NodeExt = function(){
 
-    /** @type {EU.EventsList} */
-    _events : null,
-    /** @type {cc.Action} */
-    _actions: null,
-    /** @type {EU.ParamCollection} */
-    _params: null,
-    ctor: function(){
-    },
-    as_node_ref: function()
+    /** @type {map<EU.EventsList>} */
+    this._events = {};
+    /** @type {map<cc.Action>} */
+    this._actions = {};
+    /** @type {EU.ParamCollection} _params*/
+    this._params = null;
+    this.as_node_ref= function()
     {
-        return (this instanceof Node ? this : null);
-    },
-    getChildByPath_str: function( path_names )
+        return (this instanceof cc.Node ? this : null);
+    };
+    this.getChildByPath_str= function( path_names )
     {
         var self = this.as_node_ref();
+        //TODO: EU.Common
         var result = self ? EU.Common.getNodeByPath( self, path_names ) : null;
         return result;
-    },
-    getChildByPath_arr: function(path_tags )
+    };
+    this.getChildByPath_arr= function(path_tags )
     {
         var self = this.as_node_ref();
+        //TODO: EU.Common
         var result = self ? EU.Common.getNodeByTagsPath(self, path_tags) : null;
         return result;
-    },
-    getParamCollection: function()
+    };
+    this.getParamCollection= function()
     {
         return this._params;
-    },
+    };
     /**
      * @param {String} path
      * @param {String} xmlFile
      */
-    load_str_n_str: function( path, xmlFile )
+    this.load_str_n_str= function( path, xmlFile )
     {
         var file = path;
         if( !EU.xmlLoader.isEmpty(file) && file.substr(-1) != '/' )
             file += '/';
         file += xmlFile;
         this.load_str( file );
-    },
-    load_str: function(file )
+    };
+    this.load_str= function(file )
     {
         if( EU.xmlLoader.isEmpty(file)) return;
         var xmlnode = new EU.pugixml.readXml(file);
         var root = xmlnode.firstElementChild;
         this.load_xmlnode( root );
-    },
-    load_xmlnode: function( root )
+    };
+    this.load_xmlnode= function( root )
     {
         EU.xmlLoader.bookDirectory( this );
         EU.xmlLoader.load( this, root );
         EU.xmlLoader.unbookDirectory();
-    },
-    runEvent: function( eventname )
+    };
+    this.runEvent= function( eventname )
     {
         var iter = this._events[eventname];
         if( iter != null )
@@ -66,26 +66,26 @@ EU.NodeExt = cc.Class.extend({
             var name = this.as_node_ref() ? EU.Node.prototype.getName.call(this) : "Not node inherited";
             //log_once( "NodeExt[%s]: event with name [%s] not dispatched", name.c_str( ), eventname.c_str( ) );
         }
-    },
-    getAction: function( name )
+    };
+    this.getAction= function( name )
     {
         var iter = this._actions[name];
         if( iter != null )
             return iter;
         return null;
-    },
-    setProperty_int: function( intproperty, value )
+    };
+    this.setProperty_int= function( intproperty, value )
     {
         return false;
-    },
-    setProperty_str: function( stringproperty, value )
+    };
+    this.setProperty_str= function( stringproperty, value )
     {
         return false;
-    },
+    };
     /**
      * @param {Element} xmlnode
      */
-    loadActions: function( xmlnode )
+    this.loadActions= function( xmlnode )
     {
         for(var i=0; i < xmlnode.children.length; i++){
             var xmlaction = xmlnode.children[i];
@@ -94,11 +94,9 @@ EU.NodeExt = cc.Class.extend({
             EU.assert( action );
             this._actions[actionname] = action;
         }
-    },
-    /**
-     * @param {Element} xmlnode
-     */
-    loadEvents: function(xmlnode )
+    };
+    /**  @param {Element} xmlnode */
+    this.loadEvents= function(xmlnode )
     {
         for(var i=0; i < xmlnode.children.length; i++){
             var xmllist = xmlnode.children[i];
@@ -110,18 +108,21 @@ EU.NodeExt = cc.Class.extend({
                 this._events[listname].push( event );
             }
         }
-    },
-    loadParams: function( xmlnode )
+    };
+    /**  @param {Element} xmlnode */
+    this.loadParams= function( xmlnode )
     {
         for(var i=0; i < xmlnode.children.length; i++){
             var xmlparam = xmlnode.children[i];
             var name = xmlparam.tagName;
             var attr = xmlparam.getAttribute( "value" );
             var value = attr ? attr : xmlparam.text();
-            this._params[name] = value;
+            this._params.set(name, value);
         }
-    },
-    loadXmlEntity: function(tag, xmlnode )
+    };
+    /**  @param {String} tag 
+     * @param {Element} xmlnode */
+    this.loadXmlEntity= function(tag, xmlnode )
     {
         if( tag == EU.k.xmlTag.ParamCollection )
         {
@@ -129,20 +130,11 @@ EU.NodeExt = cc.Class.extend({
         }
         return false;
     }
-});
+};
 
 
 EU.LayerExt = cc.Layer.extend();
-EU.LayerExt.implement(EU.NodeExt.prototype);
-cc.extend(EU.LayerExt, function() {
-    //DECLARE_BUILDER( LayerExt );
-    this.ctor = function () {
-    }
-});
+EU.NodeExt.call(EU.LayerExt.prototype);
 
 EU.NodeExt_ = cc.Node.extend();
-cc.extend(EU.NodeExt_, cc.NodeExt);
-cc.extend(EU.NodeExt_, function() {
-    //DECLARE_BUILDER( NodeExt_ );
-    this.init = function () { return true; }
-});
+EU.NodeExt.call(EU.NodeExt_.prototype);
