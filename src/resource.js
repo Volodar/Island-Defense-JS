6f1ef94f7d45b2c3daa5c3940c8b7cb691c6716c
VolodarDev <Bitbucket112358>
2016-02-02 09:44:04 +0600

    resources, configurations

--- src/resource.js
+++ src/resource.js
@@ -1,9 +1,10 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    MainScene_json : "res/MainScene.json",
+    MainScene_json : "images/maings/title_20481536.jpg",
+    MapBG : "images/map/Friendly_Map.jpg",
 };
 
 var g_resources = [];
 for (var i in res) {
-    g_resources.push(res[i]);
+    var resRoot = "res/_origin/";
+    g_resources.push( resRoot + res[i]);
 }
