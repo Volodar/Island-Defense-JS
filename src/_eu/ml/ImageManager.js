031f72d1b5d3be942f33116d5d50a08eeff919bb
VolodarDev <Bitbucket112358>
2016-02-12 18:58:29 +0600

    rollback some changes

--- src/_eu/ml/ImageManager.js
+++ src/_eu/ml/ImageManager.js
@@ -72,7 +72,7 @@ EU.ImageManager  = {
             frame = this.frames[spriteFrameOrTexture];
         if( frame )
         {
-            sprite = new cc.SpriteFrame( frame );
+            sprite = new cc.Sprite( frame );
         }
         else
         {
